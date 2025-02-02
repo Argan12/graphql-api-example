0563a71a916d1a8b7b72bd21580a2e08147d77cc
Argan12 <argan.piquet@gmail.com>
2025-02-02 15:05:43 +0100

    :fire: Remove annotations

--- src/GraphQLApiExample.Application/Features/User/Types/Inputs/CreateUserInput.cs
+++ src/GraphQLApiExample.Application/Features/User/Types/Inputs/CreateUserInput.cs
@@ -4,12 +4,8 @@ namespace GraphQLApiExample.Application.Features.User.Types.Inputs
 {
     public class CreateUserInput
     {
-        [Required]
-        [MaxLength(100)]
         public string Pseudo { get; set; } = null!;
 
-        [Required]
-        [EmailAddress]
         public string Mail { get; set; } = null!;
     }
 }
